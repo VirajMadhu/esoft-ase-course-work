100b28dacb7665a1b919b2a8418e7b20c64348dd
zainab878 <zainabbanu878@gmail.com>
2026-02-10 21:53:19 +0530

    Imported account and auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import routes from "./routes/index.js";
+import accountRoutes from "./routes/account.routes.js";
+import authRoutes from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -9,6 +11,9 @@ const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/api/account", accountRoutes);
+app.use("/api/auth", authRoutes);
+
 
 // health route
 app.use("/api", routes);
