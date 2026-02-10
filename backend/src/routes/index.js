80f46a4babbc878f8040f524f2f686e38dff43e9
zainab878 <zainabbanu878@gmail.com>
2026-02-10 19:34:25 +0530

    modified index

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ import productsRouter from "./products.routes.js";
 import constantDataRouter from "./constant-data.routes.js";
 import authRouter from "./auth.routes.js";
 import ordersRouter from "./orders.routes.js";
+import accountRouter from "./account.routes.js";
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.use("/products", productsRouter);
 router.use("/constants", constantDataRouter);
 router.use("/auth", authRouter);
 router.use("/orders", ordersRouter);
+router.use("/account", accountRouter);
 
 export default router;
